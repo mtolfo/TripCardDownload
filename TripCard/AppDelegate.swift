b3d1b019d1db53ec0bbc0c501e9aba336f56ca05
Michael Tolfo <mtolfo@mac.com>
2016-01-05 19:00:05 -0500

    Added a comment in the page for testing.

--- TripCard/AppDelegate.swift
+++ TripCard/AppDelegate.swift
@@ -12,6 +12,7 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    // I am typing this to test github and to learn source control
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool
